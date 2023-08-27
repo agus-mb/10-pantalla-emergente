58f9206efcdc4ef86576dd972f0001f70cfbfa93
agus-mb <munosagustina3@gmail.com>
2023-08-27 18:56:31 -0300

    finish

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const botonReducir= document.querySelector('.boton-desactivador');
 const mensaje=document.querySelector('.contenedor-emergente');
 const pantallaNegra=document.querySelector('.pantalla-negra');
 
-
-
 mensaje.classList.add('desactivado');
 
 boton.addEventListener('click', () => {
     mensaje.classList.toggle('desactivado');
 pantallaNegra.classList.add('overlay')
+})
+
+botonReducir.addEventListener('click', () => {
+    mensaje.classList.toggle('desactivado');
+pantallaNegra.classList.remove('overlay')
 })
