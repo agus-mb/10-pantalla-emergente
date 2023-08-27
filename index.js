1441489c2a1571428a0b74c3ca665ddbe9589a6b
agus-mb <munosagustina3@gmail.com>
2023-08-26 23:03:59 -0300

    Estructura basica

---
+++ index.js
@@ -0,0 +1,13 @@
+const boton= document.querySelector('.boton-activador');
+const botonReducir= document.querySelector('.boton-desactivador');
+const mensaje=document.querySelector('.contenedor-emergente');
+const pantallaNegra=document.querySelector('.pantalla-negra');
+
+
+
+mensaje.classList.add('desactivado');
+
+boton.addEventListener('click', () => {
+    mensaje.classList.toggle('desactivado');
+pantallaNegra.classList.add('overlay')
+})
